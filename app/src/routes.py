5e0c0eb2cc69410cd387218495d4c1c11df23029
VictorHenrich <victorhenrich993@gmail.com>
2022-09-10 11:17:12 -0300

    Corrigido problema em url de rota

--- app/src/routes.py
+++ app/src/routes.py
@@ -9,4 +9,4 @@ from src.controllers.http.autenticacao import AutenticacaoController
 
 server.api.add_resource(AutenticacaoController, '/autenticacao')
 server.api.add_resource(CrudProdutosController, '/produtos/crud', '/produtos/crud/<uuid:hash_produto>')
-server.api.add_resource(CrudGruposProdutoController, '/grupos_produto/crud', 'grupos_produto/crud/<uuid:hash_grupo>')
+server.api.add_resource(CrudGruposProdutoController, '/grupos_produto/crud', '/grupos_produto/crud/<uuid:hash_grupo>')
